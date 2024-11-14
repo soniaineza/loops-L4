f31d51008b99b78d88d52a5a479dd40ab6626ce4
soniaineza <ntwariprince013@gmail.com>
2024-11-14 06:48:07 +0200

    loops

---
+++ Table of 23.py
@@ -0,0 +1,4 @@
+print("table of 23 ")
+for i in range(1,11):
+    mul = 23 * i 
+    print("23 * ",i,"=",mul)
---
+++ sum of natural numbers.py
@@ -0,0 +1,6 @@
+i = 1
+total = 0
+while(i<=10):
+    total = total + i
+    i = i+1
+print("sum of first ten natural numbers",total)
